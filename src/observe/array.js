63fefdcb6521c56f08ff9cee4a075db126db3164
sunnshare <1534414811@qq.com>
2023-12-11 21:13:09 +0800

    实现数组的函数劫持

---
+++ src/observe/array.js
@@ -0,0 +1,31 @@
+// 重写数组中的部分方法
+
+let oldArrayProto = Array.prototype;
+// newArrayProto.__proto__ = oldArrayProto
+export let newArrayProto = Object.create(oldArrayProto);
+
+let methods = ["push", "pop", "shift", "unshift", "reverse", "sort", "splice"]; // 改变原数组的方法
+
+methods.forEach((method) => {
+  newArrayProto[method] = function (...args) {
+    const result = oldArrayProto[method].call(this, ...args); // 内部调用原来的方法，函数的劫持，切片编程
+
+    // 对新增的对象数据再次进行劫持
+    let inserted;
+    let ob = this.__ob__;
+    switch (method) {
+      case "push":
+      case "unshift":
+        inserted = args;
+        break;
+      case "splice":
+        inserted = args.slice(2);
+      default:
+        break;
+    }
+    if (inserted) {
+      ob.observeArray(inserted); // 对新增的内容再次进行观测
+    }
+    return result;
+  };
+});
--- src/observe/index.js
+++ src/observe/index.js
@@ -1,11 +1,27 @@
+import { newArrayProto } from "./array";
 class Observer {
   constructor(data) {
-    this.walk(data);
+    Object.defineProperty(data, "__ob__", {
+      value: this,
+      enumerable: false, // 将 __ob__ 变成不可枚举对象，循环时找不到
+    });
+
+    // data.__ob__ = this; // 数据上写__ob__保存Observer类，用于后续调用观测数组方法，防止重复观测
+    if (Array.isArray(data)) {
+      data.__proto__ = newArrayProto;
+      this.observeArray(data);
+    } else {
+      this.walk(data);
+    }
   }
 
   walk(data) {
     // 循环对象 对属性依次劫持，重新定义属性
     Object.keys(data).forEach((key) => difineReactive(data, key, data[key]));
+  }
+
+  observeArray(data) {
+    data.forEach((item) => observe(item)); // 监测数组中的对象
   }
 }
 
@@ -26,6 +42,9 @@ export function difineReactive(target, key, value) {
 export function observe(data) {
   if (typeof data !== "object" || data == null) {
     return; // 只对对象进行劫持
+  }
+  if (data.__ob__ instanceof Observer) {
+    return data.__ob__;
   }
 
   return new Observer(data);
