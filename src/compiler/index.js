c82a9dbda31eef8b361465b27570b82a215cd962
sunnshare <1534414811@qq.com>
2023-12-12 23:03:26 +0800

    代码生成实现原理

--- src/compiler/index.js
+++ src/compiler/index.js
@@ -1,6 +1,66 @@
 import { parseHTML } from "./parse";
+const defaultTagRE = /\{\{((?:.|\r?\n)+?)\}\}/g;
+
+function genProps(attrs) {
+  let str = "";
+  for (let i = 0; i < attrs.length; i++) {
+    let attr = attrs[i];
+    if (attr.name === "style") {
+      let obj = {};
+      attr.value.split(";").forEach((item) => {
+        let [key, value] = item.split(":");
+        obj[key.trim()] = value.trim();
+      });
+      attr.value = obj;
+    }
+    str += `${attr.name}:${JSON.stringify(attr.value)},`;
+  }
+  return `{${str.slice(0, -1)}}`;
+}
+
+function gen(node) {
+  if (node.type === 1) {
+    return codegen(node);
+  } else {
+    let text = node.text;
+    if (!defaultTagRE.test(text)) {
+      return `_v(${JSON.stringify(text)})`;
+    } else {
+      let tokens = [];
+      let match;
+      defaultTagRE.lastIndex = 0;
+      let lastIndex = 0;
+      while ((match = defaultTagRE.exec(text))) {
+        let index = match.index;
+        if (index > lastIndex) {
+          tokens.push(JSON.stringify(text.slice(lastIndex, index)));
+        }
+        tokens.push(`_s(${match[1].trim()})`);
+        lastIndex = index + match[0].length;
+      }
+      if (lastIndex < text.length) {
+        tokens.push(JSON.stringify(text.slice(lastIndex)));
+      }
+      return `_v(${tokens.join("+")})`;
+    }
+  }
+}
+
+function genChildren(children) {
+  return children.map((child) => gen(child)).join(",");
+}
+
+function codegen(ast) {
+  let children = genChildren(ast.children);
+  let code = `_c('${ast.tag}',${
+    ast.attrs.length > 0 ? genProps(ast.attrs) : "null"
+  }${ast.children.length ? `,${children}` : ""})`;
+  return code;
+}
+
 export function compileToFunction(template) {
   // 1.将template转化成ast语法树
   let ast = parseHTML(template);
   // 2.生成render方法
+  let code = codegen(ast);
 }
